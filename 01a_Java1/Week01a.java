18afc12d91326ff30f25ee9374fae948fd97d0f0
randyfortier <randy.fortier@gmail.com>
2019-09-09 14:00:55 -0400

    First lecture demo

---
+++ 01a_Java1/Week01a.java
@@ -0,0 +1,24 @@
+/**
+ * main
+ *
+ * This is the in-class demo from the first lecture
+ * 
+ * @author Randy Fortier
+ * @date September 9, 2019
+ */
+public class Week01a {
+    static boolean teamEvil = true;
+    int hp = 10;
+
+    /**
+     * main
+     * 
+     * The entry point for the program
+     *
+     * @arg args The command-line arguments
+     */
+     public static void main(String[] args) {
+         System.out.println("teamEvil = " + teamEvil);
+         //System.out.println("hp = " + hp);
+     }
+}
